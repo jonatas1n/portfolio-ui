a404a9928643f708575124099d526513407da897
jonatas1n <jonatasgeek@gmail.com>
2025-02-09 22:43:32 -0300

    feat: set fullscreen to first section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,15 @@ import "@fontsource-variable/source-sans-3";
 export default function Home() {
   return (
     <Container>
-      <div className="grid grid-cols-6 gap-6 h-full py-6 min-h-dvh items-start">
+      <div className="grid grid-cols-6 gap-6 h-full min-h-dvh items-start">
         <div className="col-span-5 grid gap-32 justify-center">
-          <Intro />
-          <div className="w-full text-accent flex justify-center text-center">
-            <ChevronDown />
+          <div className="h-dvh grid-rows-3 gap-4 grid items-center justify-center -mb-60">
+            <div className="row-start-2">
+              <Intro />
+            </div>
+            <div className="w-full row-start-3 text-accent flex justify-center text-center">
+              <ChevronDown />
+            </div>
           </div>
           <Projects />
           <Skills />
