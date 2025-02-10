067fa8908cbcce6957198cc4b54901d28bbd4793
jonatas1n <jonatasgeek@gmail.com>
2025-02-09 22:14:00 -0300

    chore: change from children to description prop

--- src/app/sections/Skills/index.tsx
+++ src/app/sections/Skills/index.tsx
@@ -2,34 +2,14 @@ import { OutlineCard } from "@/app/components/OutlineCard";
 import { SectionCard } from "@/app/components/SectionCard";
 
 export const Skills = () => {
+  const description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quasi, eaque fugiat saepe quibusdam exercitationem. Placeat nulla dicta quam voluptatum, mollitia accusantium dignissimos. Labore exercitationem mollitia eligendi iste, repudiandae delectus quas ad ullam cupiditate quod tempore ex eos, doloribus dolore qui aliquam porro voluptatibus repellendus?"
   return (
     <SectionCard>
       <div className="grid gap-8">
         <h3 className="text-4xl font-display font-bold">Skills</h3>
-        <OutlineCard title="Typescript">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
-          quasi, eaque fugiat saepe quibusdam exercitationem. Placeat nulla
-          dicta quam voluptatum, mollitia accusantium dignissimos. Labore
-          exercitationem mollitia eligendi iste, repudiandae delectus quas ad
-          ullam cupiditate quod tempore ex eos, doloribus dolore qui aliquam
-          porro voluptatibus repellendus?
-        </OutlineCard>
-        <OutlineCard title="React">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
-          quasi, eaque fugiat saepe quibusdam exercitationem. Placeat nulla
-          dicta quam voluptatum, mollitia accusantium dignissimos. Labore
-          exercitationem mollitia eligendi iste, repudiandae delectus quas ad
-          ullam cupiditate quod tempore ex eos, doloribus dolore qui aliquam
-          porro voluptatibus repellendus?
-        </OutlineCard>
-        <OutlineCard title="Python">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
-          quasi, eaque fugiat saepe quibusdam exercitationem. Placeat nulla
-          dicta quam voluptatum, mollitia accusantium dignissimos. Labore
-          exercitationem mollitia eligendi iste, repudiandae delectus quas ad
-          ullam cupiditate quod tempore ex eos, doloribus dolore qui aliquam
-          porro voluptatibus repellendus?
-        </OutlineCard>
+        <OutlineCard title="Typescript" description={description} />
+        <OutlineCard title="React" description={description} />
+        <OutlineCard title="Python" description={description} />
       </div>
     </SectionCard>
   );
