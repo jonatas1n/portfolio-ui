877272086829911e2810dbafe720a6292e856831
jonatas1n <jonatasgeek@gmail.com>
2025-02-09 23:12:22 -0300

    chore: add links and icons to sidemenu options

--- src/app/components/SideMenu/index.tsx
+++ src/app/components/SideMenu/index.tsx
@@ -1,12 +1,28 @@
+import { social } from "@/app/constants";
+import { Github, LucideLinkedin, Mail } from "lucide-react";
+import Link from "next/link";
+
 export const SideMenu = () => {
   return (
-    <div className="text-light font-display col-span-1 fixed top-1/2 transform -translate-y-1/2">
-      <p>Translation button</p>
-      <strong>Get in touch</strong>
-      <ul>
-        <li>Github</li>
-        <li>LinkedIn</li>
-        <li>Email</li>
+    <div className="text-light font-display gap-3 grid col-span-1 fixed top-1/2 transform -translate-y-1/2">
+      
+      <p className="font-bold">Get in touch</p>
+      <ul className="grid gap-3 font-body">
+        <Link className="underline" href={social.github}>
+          <li className="flex gap-1 items-center">
+            <Github size={20}/> Github
+          </li>
+        </Link>
+        <Link className="underline" href={social.linkedin}>
+          <li className="flex gap-1">
+            <LucideLinkedin /> LinkedIn
+          </li>
+        </Link>
+        <Link className="underline" href={social.email}>
+          <li className="flex gap-1">
+            <Mail /> Email
+          </li>
+        </Link>
       </ul>
     </div>
   );
---
+++ src/app/constants.ts
@@ -0,0 +1,5 @@
+export const social = {
+  github: "https://www.github.com/jonatas1n",
+  linkedin: "https://www.linkedin.com/in/jonatasgomesdev",
+  email: "mailto:jonatasgomesb@gmail.com",
+};
