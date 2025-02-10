547f88547ad8af20f22968c55e5f28061ea15c19
jonatas1n <jonatasgeek@gmail.com>
2025-02-10 11:40:42 -0300

    chore: adjust margin to project excess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,17 +14,19 @@ import { Footer } from "./sections/Footer";
 export default function Home() {
   return (
     <Container>
-      <div className="grid grid-cols-6 gap-6 h-full min-h-dvh items-start mb-24">
+      <div className="grid grid-cols-6 gap-6 h-full items-start mb-24">
         <div className="col-span-5 grid gap-32 justify-center">
-          <div className="h-dvh grid-rows-3 gap-4 grid items-center justify-center -mb-60">
+          <div className="h-dvh grid-rows-3 gap-4 grid items-center justify-center">
             <div className="row-start-2">
               <Intro />
             </div>
-            <div className="w-full row-start-3 text-accent flex justify-center text-center">
+            <div className="w-full row-start-3 m-30 text-accent flex justify-center text-center">
               <ChevronDown />
             </div>
           </div>
-          <Projects />
+          <div className="-mt-60">
+            <Projects />
+          </div>
           <Skills />
           <Experiences />
         </div>
