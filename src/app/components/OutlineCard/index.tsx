3cc1551c5548290f595f01f5ed155b33e37bd087
jonatas1n <jonatasgeek@gmail.com>
2025-02-09 22:13:23 -0300

    chore: change from children to description prop

--- src/app/components/OutlineCard/index.tsx
+++ src/app/components/OutlineCard/index.tsx
@@ -1,22 +1,15 @@
-import { PropsWithChildren } from "react";
-
 type OutlineCardProps = {
   title: string;
+  description: string;
 };
 
-export const OutlineCard = ({
-  children,
-  title,
-}: PropsWithChildren<OutlineCardProps>) => {
+export const OutlineCard = ({ description, title }: OutlineCardProps) => {
   return (
     <div className=" border rounded-3xl border-dark p-6">
-      <h4 className="bg-light flex absolute px-4 font-bold text-2xl -translate-y-11">
+      <h4 className="bg-light flex absolute font-display px-4 font-bold text-2xl -translate-y-11">
         {title}
       </h4>
-      <p>
-        {children}
-        
-      </p>
+      <p className="font-body font-medium">{description}</p>
     </div>
   );
 };
