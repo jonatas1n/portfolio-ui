99a013e155dd50e943da2ea71e161c1110a524aa
jonatas1n <jonatasgeek@gmail.com>
2025-02-10 02:22:08 -0300

    chore: define source font as default to all components

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export const Button = ({ children }: PropsWithChildren) => {
   return (
-    <button className="bg-accent shadow-xl rounded-lg py-1 px-5 font-semibold flex gap-1 items-center text-light">
+    <button className="bg-accent shadow-xl rounded-lg py-1 px-5 font-semibold flex gap-1 items-center text-light font-display">
       {children}
     </button>
   );
