2e311962ff51de876577ff7d91010bb0de032ffb
jonatas1n <jonatasgeek@gmail.com>
2025-02-15 16:23:27 -0300

    chore: align back to top button

--- src/app/components/BackToTop/index.tsx
+++ src/app/components/BackToTop/index.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import { CircleArrowUp } from "lucide-react";
+import { FaCircleArrowUp } from "react-icons/fa6";
 
 export const BackToTop = () => {
   return (
-    <div className=" text-light absolute bottom-0">
-      <Link className="flex gap-4" href="#home">
+    <div className=" text-light sm:bottom-0 bottom-auto absolute md:justify-self-auto justify-self-center">
+      <Link className="flex gap-4 items-center" href="#home">
         Back to top
         <FaCircleArrowUp />
       </Link>
