cc5f7d5fc11d1836f78e3d3afd5867b53b10fd27
jonatas1n <jonatasgeek@gmail.com>
2025-02-15 17:09:45 -0300

    feat: replace card by skill button

---
+++ src/app/sections/Skills/constants.ts
@@ -0,0 +1,10 @@
+import { FaDocker, FaPython, FaReact } from "react-icons/fa"
+import { SiJavascript, SiTypescript } from "react-icons/si"
+
+export const skillIcons = {
+  react: FaReact,
+  docker: FaDocker,
+  typescript: SiTypescript,
+  python: FaPython,
+  javascript: SiJavascript,
+}
--- src/app/sections/Skills/index.tsx
+++ src/app/sections/Skills/index.tsx
@@ -1,18 +1,54 @@
-import { getSkills } from "@/app/services";
-import { SkillCard } from "./SkillCard";
-import { SectionCard } from "@/app/components/SectionCard";
+"use client";
 
-export const Skills = async () => {
-  const skills = await getSkills();
-  
+import { getSkills } from "@/app/services";
+import { SectionCard } from "@/app/components/SectionCard";
+import { useEffect, useState } from "react";
+import { Skill } from "@/app/types";
+import { Modal } from "@/app/components/Modal";
+import { SkillButton } from "./SkillButton";
+
+export const Skills = () => {
+  const [skills, setSkills] = useState<Skill[]>([]);
+  const [modalContent, setModalContent] = useState<
+    Omit<Skill, "id"> | undefined
+  >(undefined);
+
+  const clearModalContent = () => setModalContent(undefined);
+
+  useEffect(() => {
+    getSkills().then((fetchedSkills) => {
+      setSkills(fetchedSkills);
+    });
+  });
+
   return (
     <SectionCard id="skills">
-      <div className="grid gap-8">
+      {modalContent && (
+        <Modal
+          title={modalContent.title}
+          isOpen={!!modalContent}
+          onClose={clearModalContent}
+        >
+          {modalContent.description}
+        </Modal>
+      )}
+      <div className="grid gap-6">
         <h3 className="text-4xl font-display font-bold">Skills</h3>
-        {skills?.map(skill => (
-          <SkillCard key={skill.id} {...skill} />
-        ))}
-        {skills?.length === 0 && <p className="text-lg">No skills found</p>}
+        <div className="flex gap-10">
+          {skills?.map((skill) => (
+            <SkillButton
+              key={skill.id}
+              skill={skill.title}
+              onClick={() =>
+                setModalContent({
+                  title: skill.title,
+                  description: skill.description,
+                })
+              }
+            />
+          ))}
+          {skills?.length === 0 && <p className="text-lg">No skills found</p>}
+        </div>
       </div>
     </SectionCard>
   );
