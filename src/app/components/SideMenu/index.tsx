8a7d9f5aff8cc8e2f27af59a97ed0fc1eb73e8e7
jonatas1n <jonatasgeek@gmail.com>
2025-02-16 11:15:32 -0300

    feat: animate social media

--- src/app/components/SideMenu/index.tsx
+++ src/app/components/SideMenu/index.tsx
@@ -2,11 +2,15 @@
 
 import { useState } from "react";
 import { social } from "@/app/constants/social";
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FiMail } from "react-icons/fi";
 import Link from "next/link";
 import { FloatButton } from "../FloatButton";
+import * as motion from "motion/react-client";
+
+const sectionsLinks = [
+  { link: "#projects", label: "Creations" },
+  { link: "#skills", label: "Skills" },
+  { link: "#experiences", label: "Journey" },
+];
 
 export const SideMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -24,41 +28,46 @@ export const SideMenu = () => {
         } lg:flex flex-col-reverse lg:flex-col transition font-display gap-5 lg:bg-transparent bg-accent lg:p-0 p-4 rounded-xl lg:rounded-none`}
       >
         <ul className="grid gap-2">
-          <Link href="#projects">
-            <li className="bg-light text-dark py-1 px-4 hover:text-light hover:bg-accent rounded-xl font-semibold text-lg">
-              Projects
-            </li>
-          </Link>
-          <Link href="#skills">
-            <li className="bg-light text-dark py-1 px-4 hover:text-light hover:bg-accent rounded-xl font-semibold text-lg">
-              Skills
-            </li>
-          </Link>
-          <Link href="#experiences">
-            <li className="bg-light text-dark py-1 px-4 hover:text-light hover:bg-accent rounded-xl font-semibold text-lg">
-              Experiences
-            </li>
-          </Link>
+          {sectionsLinks.map((link) => (
+            <Link key={link.link} href="#projects">
+              <motion.li
+                initial={{ opacity: 0, scale: 1.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                whileHover={{ scale: 1.125 }}
+                className="bg-light text-dark py-1 px-4 hover:text-light hover:bg-accent rounded-xl font-semibold text-lg"
+              >
+                {link.label}
+              </motion.li>
+            </Link>
+          ))}
         </ul>
         <hr />
         <div className="text-light gap-3 grid">
-          <p className="font-bold">Get in touch</p>
+          <motion.p
+            initial={{ opacity: 0, transform: "translateX(1rem)" }}
+            animate={{ opacity: 1, transform: "translateX(0)" }}
+            className="font-bold"
+          >
+            Get in touch
+          </motion.p>
           <ul className="flex justify-between font-body">
-            <Link target="__blank" className="underline" href={social.github}>
-              <li className="flex hover:bg-accent hover:text-light rounded-full bg-light text-dark p-1">
-                <FaGithub size={18} />
-              </li>
-            </Link>
-            <Link target="__blank" className="underline" href={social.linkedin}>
-              <li className="flex hover:bg-accent hover:text-light rounded-full bg-light text-dark p-1">
-                <FaLinkedin size={18} />
-              </li>
-            </Link>
-            <Link className="underline" href={social.email}>
-              <li className="flex hover:bg-accent hover:text-light rounded-full bg-light text-dark p-1">
-                <FiMail size={18} />
-              </li>
-            </Link>
+            {Object.values(social).map((social, index) => (
+              <Link
+                key={social.link}
+                target="__blank"
+                className="underline"
+                href={social.link}
+              >
+                <motion.li
+                  initial={{ opacity: 0, transform: "translateY(1.5rem)" }}
+                  animate={{ opacity: 1, transform: "translateY(0)" }}
+                  transition={{ delay: 0.06125 * index }}
+                  className="flex hover:bg-accent hover:text-light rounded-full bg-light text-dark p-1"
+                >
+                  {social.icon({})}
+                </motion.li>
+              </Link>
+            ))}
           </ul>
         </div>
       </div>
