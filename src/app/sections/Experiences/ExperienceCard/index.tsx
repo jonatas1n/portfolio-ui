23d5211b3a84d1e403f3b54114f46125ac4bd86f
jonatas1n <jonatasgeek@gmail.com>
2025-02-21 01:54:40 -0300

    fix: adjust responsiviness on Experience timeline

--- src/app/sections/Experiences/ExperienceCard/index.tsx
+++ src/app/sections/Experiences/ExperienceCard/index.tsx
@@ -2,6 +2,7 @@ import { Experience } from "@/app/types";
 import { Tag } from "@/app/components/Tag";
 import { Card } from "@/app/components/Card";
 import DOMPurify from "dompurify";
+import * as motion from "motion/react-client";
 
 export const ExperienceCard = ({
   position,
@@ -12,28 +13,36 @@ export const ExperienceCard = ({
   description,
 }: Experience) => {
   return (
-    <Card>
-      <div className="grid gap-3 mx-4">
-        <div className="grid gap-1">
-          <h4 className="text-2xl font-display font-bold">{position}</h4>
-          <div className="text-xl">
-            at <strong>{companyName}</strong>
+    <motion.div
+      whileInView={{ opacity: 1, x: 0 }}
+      initial={{ opacity: 0, x: 50 }}
+      transition={{ duration: 0.5 }}
+    >
+      <Card>
+        <div className="grid gap-3">
+          <div className="grid gap-1">
+            <h4 className="md:text-2xl text-xl font-display font-bold">
+              {position}
+            </h4>
+            <div className="md:text-xl text-lg">
+              at <strong>{companyName}</strong>
+            </div>
+            <div className="md:text-lg text-md">
+              {startDate} ~ {endDate ?? "Actually"}
+            </div>
           </div>
-          <div className="text-lg">
-            {startDate} ~ {endDate ?? "Actually"}
+          <div className="flex flex-wrap gap-4 pb-4 border-b border-b-accent">
+            {technologies.map((technology: string) => (
+              <Tag key={technology}>{technology}</Tag>
+            ))}
           </div>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(description),
+            }}
+          />
         </div>
-        <div className="flex flex-wrap gap-4 pb-4 border-b border-b-accent">
-          {technologies.map((technology: string) => (
-            <Tag key={technology}>{technology}</Tag>
-          ))}
-        </div>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: DOMPurify.sanitize(description),
-          }}
-        />
-      </div>
-    </Card>
+      </Card>
+    </motion.div>
   );
 };
--- src/app/sections/Experiences/ExperiencesTimeline/index.tsx
+++ src/app/sections/Experiences/ExperiencesTimeline/index.tsx
@@ -10,20 +10,18 @@ export const ExperiencesTimeline = ({
   experiencesList,
 }: ExperiencesTimelineProps) => {
   return (
-    <div className="grid grid-flow-col gap-6">
-      <div className="border-r-4 border-card pl-4" />
+    <div className="grid grid-flow-col gap-6 relative">
+      <div className="border-r-4 border-card absolute h-full w-1 z-0" />
       <div className="grid gap-10 pt-6">
         {experiencesList.map((experience) => (
-          <motion.div
-            whileInView={{ opacity: 1, x: -38 }}
-            initial={{ opacity: 0, x: 50 }}
-            transition={{ duration: 0.5 }}
-            key={experience.id}
-            className="grid grid-flow-col gap-6 -translate-x-[2.375rem]"
-          >
-            <div className="rounded-full w-6 h-6 bg-dark border-accent border-2 mt-4" />
+          <div key={experience.id} className="grid grid-flow-col md:gap-4 z-10">
+            <motion.div
+              whileInView={{ opacity: 1 }}
+              initial={{ opacity: 0 }}
+              className="rounded-full md:w-6 md:h-6 w-4 h-4 bg-dark border-accent border-2 mt-4 md:-translate-x-[.65rem] -translate-x-[.35rem]"
+            />
             <ExperienceCard {...experience} />
-          </motion.div>
+          </div>
         ))}
       </div>
     </div>
