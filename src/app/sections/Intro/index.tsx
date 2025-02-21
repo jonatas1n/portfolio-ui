117dcc954767d68663cbbf832b8305768393aa97
jonatas1n <jonatasgeek@gmail.com>
2025-02-21 12:26:35 -0300

    feat: make intro animations better

--- src/app/sections/Intro/index.tsx
+++ src/app/sections/Intro/index.tsx
@@ -13,7 +13,7 @@ export const Intro = () => {
   const [intro, setIntro] = useState(false);
 
   return (
-    <motion.div className="h-[85dvh] min-h-[850px] lg:h-screen grid-rows-3 gap-4 grid">
+    <motion.div className="h-[85dvh] min-h-[850px] lg:h-screen grid-rows-3 gap-4 grid overflow-hidden">
       <AnimatePresence>{intro && <SoundPad />}</AnimatePresence>
       <div className="row-start-2 row-end-4 lg:row-end-3 lg:self-start self-center">
         <motion.div
@@ -22,58 +22,14 @@ export const Intro = () => {
           transition={{ duration: 0.75 }}
         >
           <SectionCard>
-            <div className="sm:grid-cols-8 grid-cols-1 grid mt-3 sm:py-16">
+            <div className="sm:grid-cols-8 grid-cols-1 grid mt-3 sm:py-16 relative min-h-52">
               <AnimatePresence>
-                {intro ? (
-                  <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    className="lg:col-span-7 col-span-full gap-6 grid"
-                  >
-                    <div className="grid sm:gap-9 gap-4 md:gap-3 md:px-0 px-2">
-                      <div className="font-display">
-                        <motion.h1
-                          initial={{ opacity: 0, y: 15 }}
-                          animate={{ opacity: 1, y: 0 }}
-                          transition={{ delay: 0.1, duration: 1 }}
-                          className="lg:text-6xl md:text-5xl text-3xl font-bold leading-10"
-                        >
-                          I&apos;m Jônatas Gomes
-                        </motion.h1>
-                        <motion.h4
-                          initial={{ opacity: 0, y: 15 }}
-                          animate={{ opacity: 1, y: 0 }}
-                          transition={{ delay: 0.4, duration: 1 }}
-                          className="sm:text-xl font-bold text-accent leading-10"
-                        >
-                          (but you can call me Johny)
-                        </motion.h4>
-                      </div>
-                      <motion.h3
-                        initial={{ opacity: 0, x: 15 }}
-                        animate={{ opacity: 1, x: 0 }}
-                        transition={{ delay: 1, duration: 1 }}
-                        className="sm:text-3xl text-xl font-bold font-display"
-                      >
-                        and this is my portfolio 👨🏿‍💻
-                      </motion.h3>
-                      <motion.div
-                        initial={{ opacity: 0, y: 15 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ delay: 1.75, duration: 1 }}
-                        className="text-base font-body py-2 md:py-4 border-y border-dark"
-                      >
-                        {BIOGRAPHY_PHRASE}
-                      </motion.div>
-                    </div>
-                  </motion.div>
-                ) : (
+                {!intro && (
                   <motion.div
                     initial={{ opacity: 0, x: -64 }}
                     animate={{ opacity: 1, x: 0 }}
-                    exit={{ opacity: 0, x: 64 }}
-                    className="flex col-span-full gap-1 font-display text-6xl font-bold justify-center"
+                    exit={{ opacity: 0, x: -64, scale: 0 }}
+                    className="flex absolute col-span-full gap-1 font-display text-6xl font-bold justify-center"
                   >
                     Hey!{" "}
                     <motion.div
@@ -86,6 +42,51 @@ export const Intro = () => {
                   </motion.div>
                 )}
               </AnimatePresence>
+              {intro && (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.5 }}
+                  className="lg:col-span-7 col-span-full gap-6 grid transition-all"
+                >
+                  <div className="grid sm:gap-9 gap-4 md:gap-3 md:px-0 px-2">
+                    <div className="font-display">
+                      <motion.h1
+                        initial={{ opacity: 0, y: 15 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ delay: 0.1, duration: 1 }}
+                        className="lg:text-6xl md:text-5xl text-3xl font-bold leading-10"
+                      >
+                        I&apos;m Jônatas Gomes
+                      </motion.h1>
+                      <motion.h4
+                        initial={{ opacity: 0, y: 15 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ delay: 0.4, duration: 1 }}
+                        className="sm:text-xl font-bold text-accent leading-10"
+                      >
+                        (but you can call me Johny)
+                      </motion.h4>
+                    </div>
+                    <motion.h3
+                      initial={{ opacity: 0, x: 15 }}
+                      animate={{ opacity: 1, x: 0 }}
+                      transition={{ delay: 1, duration: 1 }}
+                      className="sm:text-3xl text-xl font-bold font-display"
+                    >
+                      and this is my portfolio 👨🏿‍💻
+                    </motion.h3>
+                    <motion.div
+                      initial={{ opacity: 0, y: 15 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      transition={{ delay: 1.75, duration: 1 }}
+                      className="text-base font-body py-2 md:py-4 border-y border-dark"
+                    >
+                      {BIOGRAPHY_PHRASE}
+                    </motion.div>
+                  </div>
+                </motion.div>
+              )}
             </div>
           </SectionCard>
           <div className="md:flex text-light items-center gap-2 justify-center translate-y-40 hidden opacity-25">
