42c4940ae80f181dc6d4ca4f5fe6a71039c85006
jonatas1n <jonatasgeek@gmail.com>
2025-02-21 12:53:37 -0300

    refac:t move strings to constants

--- src/app/interactions/SoundPad/index.tsx
+++ src/app/interactions/SoundPad/index.tsx
@@ -2,18 +2,7 @@ import { useState, useEffect, useCallback, useRef } from "react";
 import { SoundPadItem } from "./SoundPadItem";
 import { MdTouchApp } from "react-icons/md";
 import * as motion from "motion/react-client";
-
-const NOTE_FREQUENCIES = {
-  C: { sharp: 277.18, normal: 261.63, diminished: 246.94 },
-  D: { sharp: 311.13, normal: 293.66, diminished: 277.18 },
-  E: { sharp: 349.23, normal: 329.63, diminished: 311.13 },
-  F: { sharp: 369.99, normal: 349.23, diminished: 329.63 },
-  G: { sharp: 415.3, normal: 392.0, diminished: 369.99 },
-  A: { sharp: 466.16, normal: 440.0, diminished: 415.3 },
-  B: { sharp: 523.25, normal: 493.88, diminished: 466.16 },
-};
-
-const TOUCH_MESSAGE = "Touch to interact";
+import { NOTE_FREQUENCIES, TOUCH_MESSAGE } from "./constants";
 
 export const SoundPad = () => {
   const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
