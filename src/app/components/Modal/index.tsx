b85506040efe5d294cfe2c41263a98ad4e804a55
jonatas1n <jonatasgeek@gmail.com>
2025-02-21 18:29:39 -0300

    chore: set modal content to center

--- src/app/components/Modal/index.tsx
+++ src/app/components/Modal/index.tsx
@@ -20,10 +20,10 @@ export const Modal = ({
         className="fixed w-dvw h-dvh bottom-0 top-0 left-0 right-0 z-50 backdrop-brightness-50"
       />
       <motion.div
-        initial={{ transform: "translateY(-3rem)", opacity: 0 }}
-        animate={{ transform: "translateY(0)", opacity: 1 }}
+        initial={{ translateY: "-55%", opacity: 0 }}
+        animate={{ translateY: "-50%", opacity: 1 }}
         exit={{ transform: "translateY(3rem)", opacity: 0 }}
-        className="w-1/2 h-1/2 z-50 min-h-60 fixed top-1/2 -translate-y-1/2"
+        className="w-1/2 h-1/2 z-50 min-h-60 fixed top-1/2 -translate-y-1/2 -translate-x-1/2"
       >
         <Card>
           <div className="grid gap-4">
