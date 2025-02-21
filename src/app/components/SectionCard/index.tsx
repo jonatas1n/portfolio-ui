a5d282beed5f42003f35e2e48de39c4c2dd85316
Jônatas G. <jonatasgeek@gmail.com>
2025-02-21 00:29:49 -0300

    chore: set overflow hidden to section component

--- src/app/components/SectionCard/index.tsx
+++ src/app/components/SectionCard/index.tsx
@@ -11,7 +11,7 @@ export const SectionCard = ({
   return (
     <div
       id={id}
-      className="md:px-24 shadow shadow-accent text-dark py-9 rounded-2xl bg-light px-4"
+      className="md:px-24 shadow shadow-accent text-dark py-9 rounded-2xl bg-light px-4 overflow-x-hidden"
     >
       {children}
     </div>
