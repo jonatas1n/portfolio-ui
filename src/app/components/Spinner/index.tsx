7dfa306d90dc58b5af1287d0ca53601b6f6c6f2c
jonatas1n <jonatasgeek@gmail.com>
2025-02-22 13:23:23 -0300

    chore: update spinner component

--- src/app/components/Spinner/index.tsx
+++ src/app/components/Spinner/index.tsx
@@ -2,7 +2,7 @@ import { CgSpinnerAlt } from "react-icons/cg";
 
 export const Spinner = () => {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
+    <div className="h-full w-full flex justify-center items-center">
       <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary">
         <CgSpinnerAlt />
       </div>
