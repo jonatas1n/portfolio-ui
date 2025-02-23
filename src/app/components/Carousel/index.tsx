97ac075bdbdcb9ff1c89dcf630973c8ff82d89a0
jonatas1n <jonatasgeek@gmail.com>
2025-02-23 08:31:08 -0300

    feat: set responsivity to carousel

--- src/app/components/Carousel/index.tsx
+++ src/app/components/Carousel/index.tsx
@@ -24,18 +24,18 @@ export const Carousel = ({
     <motion.div
       initial={{ opacity: 0, scale: 0.5 }}
       whileInView={{ opacity: 1, scale: 1 }}
-      className="flex gap-4 items-center justify-items-center"
+      className="grid grid-flow-col gap-4 items-center justify-items-center"
     >
       {items.length > 1 && (
         <button
-          className="justify-self-start disabled:opacity-35"
+          className="justify-self-start disabled:opacity-35 flex gap-2 items-center font-display"
           disabled={itemIndex == 0}
           onClick={prevItem}
         >
-          <FaRegArrowAltCircleLeft size={24} />
+          <FaRegArrowAltCircleLeft size={24} /> <span className="inline lg:hidden">Previous</span>
         </button>
       )}
-      <div className="grid gap-4 justify-center">
+      <div className="grid lg:row-start-1 lg:col-span-1 gap-4 justify-center row-start-2 col-start-1 col-span-2 w-full">
         {items[itemIndex]}
         {showIndex && items.length > 1 && (
           <h6 className="text-center text-dark font-display">
@@ -45,11 +45,11 @@ export const Carousel = ({
       </div>
       {items.length > 1 && (
         <button
-          className="justify-self-end disabled:opacity-35"
+          className="justify-self-end disabled:opacity-35 flex gap-2 items-center font-display"
           disabled={items.length == itemIndex + 1}
           onClick={nextItem}
         >
-          <FaRegArrowAltCircleRight size={24} />
+          <span className="inline lg:hidden">Next</span> <FaRegArrowAltCircleRight size={24} />
         </button>
       )}
     </motion.div>
