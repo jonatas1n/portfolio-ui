e3f56eeeb3e21d6f56dce36b372ba06be410c755
jonatas1n <jonatasgeek@gmail.com>
2025-02-23 02:20:51 -0300

    feat: implement new props to button component

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -1,8 +1,21 @@
 import { PropsWithChildren } from "react";
 
-export const Button = ({ children }: PropsWithChildren) => {
+type ButtonProps = {
+  onClick?: VoidFunction;
+  className?: string;
+  disabled?: boolean;
+};
+
+export const Button = ({
+  children,
+  className,
+  ...buttonProps
+}: PropsWithChildren<ButtonProps>) => {
   return (
-    <button className="bg-accent shadow-xl rounded-lg py-1 px-5 font-semibold flex gap-1 items-center text-light font-display">
+    <button
+      {...buttonProps}
+      className={`bg-accent shadow-xl rounded-lg py-1 px-5 font-semibold flex gap-1 items-center text-light font-display disabled:bg-opacity-20 ${className}`}
+    >
       {children}
     </button>
   );
