a795b104de28792eb51cff357c4f52a8125c5cc1
jonatas1n <jonatasgeek@gmail.com>
2025-03-02 12:21:50 -0300

    chore: update social constants

--- src/app/constants/social.ts
+++ src/app/constants/social.ts
@@ -1,9 +1,7 @@
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import { FiMail } from "react-icons/fi";
 
 export const social = {
   github: {link: "https://www.github.com/jonatas1n", icon: FaGithub},
   linkedin: {link: "https://www.linkedin.com/in/jonatasgomesdev", icon: FaLinkedin},
-  email: {link: "mailto:jonatasgomesb@gmail.com", icon: FiMail},
 };
