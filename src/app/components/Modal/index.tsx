99b3e2d60f353d7b7ae814af52ab58c5a7d106e9
jonatas1n <jonatasgeek@gmail.com>
2025-03-02 12:18:56 -0300

    fix: make modal component no width

--- src/app/components/Modal/index.tsx
+++ src/app/components/Modal/index.tsx
@@ -14,7 +14,7 @@ export const Modal = ({
   children,
 }: PropsWithChildren<ModalProps>) => {
   return (
-    <div className="z-50">
+    <div className="z-50 absolute">
       <div
         onClick={onClose}
         className="fixed w-dvw h-dvh bottom-0 top-0 left-0 right-0 z-50 backdrop-brightness-50"
@@ -25,17 +25,19 @@ export const Modal = ({
         exit={{ transform: "translateY(3rem)", opacity: 0 }}
         className="w-3/4 lg:w-1/2 h-1/2 z-50 min-h-60 fixed top-1/2 left-[12.5%] lg:left-1/4 -translate-y-1/2 -translate-x-[12.5%] lg:-translate-x-1/2"
       >
-        <Card>
-          <div className="grid gap-4">
-            <div className="flex w-full justify-between items-center">
-              <h1 className="text-lg font-bold font-display">{title}</h1>
-              <button onClick={onClose}>
-                <FaTimes />
-              </button>
+        <div className="grid grid-cols-6">
+          <Card className="col-span-5">
+            <div className="grid gap-4 col">
+              <div className="flex w-full justify-between items-center">
+                <h1 className="text-lg font-bold font-display">{title}</h1>
+                <button onClick={onClose}>
+                  <FaTimes />
+                </button>
+              </div>
+              {children}
             </div>
-            {children}
-          </div>
-        </Card>
+          </Card>
+        </div>
       </motion.div>
     </div>
   );
