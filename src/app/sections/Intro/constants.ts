679aa246d3ed40faa9db81d1861924904b68731c
jonatas1n <jonatasgeek@gmail.com>
2026-08-03 09:58:01 -0300

    update biography phrase

--- src/app/sections/Intro/constants.ts
+++ src/app/sections/Intro/constants.ts
@@ -1,2 +1,2 @@
 export const BIOGRAPHY_PHRASE =
-  "I am a full-stack developer passionate about creating innovative solutions, always focused on improving user experience and optimizing processes through advanced technologies and agile practices.";
+  "I am a frontend developer passionate about creating innovative solutions, always focused on improving user experience and optimizing processes through advanced technologies and agile practices.";
