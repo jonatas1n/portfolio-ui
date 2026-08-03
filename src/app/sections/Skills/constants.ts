4bd2295f6ae54673193014bf75763b6afdd8b929
jonatas1n <jonatasgeek@gmail.com>
2026-08-03 17:25:40 -0300

    update

--- src/app/sections/Skills/constants.ts
+++ src/app/sections/Skills/constants.ts
@@ -5,24 +5,34 @@ import {
   FaPython,
   FaReact,
   FaBootstrap,
+  FaAws,
 } from "react-icons/fa";
-import { FaDartLang, FaGolang } from "react-icons/fa6";
+import { FaDartLang, FaGolang, FaMasksTheater } from "react-icons/fa6";
+import { GiBearFace } from "react-icons/gi";
 import { GrGraphQl, GrMysql } from "react-icons/gr";
 import { RiNextjsFill } from "react-icons/ri";
+import { TbApi } from "react-icons/tb";
 import {
   SiChakraui,
+  SiCypress,
   SiDjango,
   SiExpress,
   SiFlask,
+  SiFlutter,
   SiJavascript,
+  SiJest,
+  SiKubernetes,
   SiMongodb,
   SiMui,
   SiPostgresql,
+  SiRedux,
   SiRubyonrails,
+  SiTestinglibrary,
   SiTypescript,
 } from "react-icons/si";
 
 export const skillIcons = {
+  react: FaReact,
   "react.js": FaReact,
   docker: FaDocker,
   typescript: SiTypescript,
@@ -35,6 +45,7 @@ export const skillIcons = {
   flask: SiFlask,
   mongodb: SiMongodb,
   postgresql: SiPostgresql,
+  postgressql: SiPostgresql,
   "node.js": FaNodeJs,
   dart: FaDartLang,
   "react native": FaReact,
@@ -45,4 +56,14 @@ export const skillIcons = {
   mysql: GrMysql,
   "chakra ui": SiChakraui,
   bootstrap: FaBootstrap,
+  "context api": TbApi,
+  redux: SiRedux,
+  zustand: GiBearFace,
+  "react testing library": SiTestinglibrary,
+  jest: SiJest,
+  cypress: SiCypress,
+  playwright: FaMasksTheater,
+  kubernetes: SiKubernetes,
+  aws: FaAws,
+  flutter: SiFlutter,
 };
